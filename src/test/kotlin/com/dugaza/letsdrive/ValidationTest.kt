b3de24232f25d0c225ba8ffa190a569d28bf957f
downingmoon <joy97q12@naver.com>
2025-01-29 19:35:29 +0900

    feat: Validation localizing
    issue#20

---
+++ src/test/kotlin/com/dugaza/letsdrive/ValidationTest.kt
@@ -0,0 +1,29 @@
+package com.dugaza.letsdrive
+
+import jakarta.validation.Validator
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
+import java.util.UUID
+import kotlin.test.assertContains
+
+@SpringBootTest
+@ActiveProfiles("test")
+class ValidationTest(
+    @Autowired val validator: Validator
+) {
+    @Test
+    fun `Spring Validation 다국어 메시지 테스트`() {
+        val vo = ValidationTestVo(UUID.randomUUID(), "")
+        val validate = validator.validate(vo)
+        val iterator = validate.iterator()
+        val messages: ArrayList<String> = ArrayList()
+        while (iterator.hasNext()) {
+            val next = iterator.next()
+            messages.add(next.message)
+            println("message: ${next.message}")
+        }
+        assertContains(messages, ("지원하지 않는 확장자입니다."))
+    }
+}
---
+++ src/test/kotlin/com/dugaza/letsdrive/ValidationTestVo.kt
@@ -0,0 +1,13 @@
+package com.dugaza.letsdrive
+
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotNull
+import java.util.UUID
+
+class ValidationTestVo(
+    @field:NotNull(message = "{FILE_001}")
+    val id: UUID,
+    @field:NotBlank(message = "{FILE_002}")
+    val title: String,
+) {
+}
